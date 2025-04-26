55f65e16a3f7ea15d0c4f1858430f9e767926c6a
Anastasov <anastasovdavid@gmail.com>
2025-04-26 12:26:17 +0200

    implemented customer api

---
+++ src/main/java/com/project/bookstore/web/CustomerAPIController.java
@@ -0,0 +1,63 @@
+package com.project.bookstore.web;
+
+import com.project.bookstore.model.Customer;
+import com.project.bookstore.model.dto.request.CustomerDTO;
+import com.project.bookstore.model.dto.response.LoyaltyPointsDTO;
+import com.project.bookstore.service.CustomerService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Getter
+@RestController
+@RequestMapping("/customer")
+public class CustomerAPIController
+{
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id)
+    {
+        Customer customer = getCustomerService().getCustomerById(id);
+        return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping({"/", "/all"})
+    public ResponseEntity<List<Customer>> getAllCustomers()
+    {
+        List<Customer> customerList = getCustomerService().getAllCustomers();
+        return ResponseEntity.ok(customerList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerDTO customerDTO)
+    {
+        Customer createdCustomer = getCustomerService().addCustomer(customerDTO);
+        return ResponseEntity.ok(createdCustomer);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer)
+    {
+        Customer updatedCustomer = getCustomerService().updateCustomer(customer);
+        return ResponseEntity.ok(updatedCustomer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id)
+    {
+        getCustomerService().deleteCustomer(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/loyalty-points")
+    public ResponseEntity<LoyaltyPointsDTO> getLoyaltyPoints(@PathVariable Long id)
+    {
+        LoyaltyPointsDTO loyaltyPointsDTO = getCustomerService().getLoyaltyPointsForCustomer(id);
+        return ResponseEntity.ok(loyaltyPointsDTO);
+    }
+}
